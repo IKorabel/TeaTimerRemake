1684a935de33517f7b51be910a0f410517581d7f
Igor Korabelnikov <i.korabel2001@gmail.com>
2022-06-09 22:49:50 +0200

    Initial Commit

---
+++ TeaTimerRemake/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TeaTimerRemake
+//
+//  Created by Игорь Корабельников on 09.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
